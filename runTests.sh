a2f42bb6ceec26c672e1e25bc234e9aeb39a3988
Lebedev Roman <lebedeffroman@gmail.com>
2018-04-11 05:40:23 +0300

    Add tests

---
+++ runTests.sh
@@ -0,0 +1,20 @@
+echo test/task1_1.in
+stack exec norm test/task1_1.in
+
+echo test/task1_2.in
+stack exec norm test/task1_2.in
+
+echo test/task1_3.in
+stack exec norm test/task1_3.in
+
+echo test/task1_4.in
+stack exec norm test/task1_4.in
+
+echo test/ack.in
+stack exec norm test/ack.in
+
+echo test/task1_oneminute.in
+stack exec norm test/task1_oneminute.in
+
+echo test/fibslow.in
+stack exec norm test/fibslow.in
